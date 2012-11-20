0449cf61bfd6f1f5778737449f621977e348b952
charonmiller08 <charonmiller@gmail.com>
2012-11-19 22:38:30 -0500

    Update src/com/db/ncsu/user/User.java
    Edited user file to reflect change on command file name.

--- src/com/db/ncsu/user/User.java
+++ src/com/db/ncsu/user/User.java
@@ -29,7 +29,7 @@ public  class User {
 		stockingCommands = new Command[]{ new AddStoreItem(), new UpdateStoreMerchandise(), new CreateMerchandise(), new CreateVendor(),
 				new UpdateVendor(), new ReviewStoreInventory(), 
 				new ShowAllVendorBills(), new UpdateSpecialOrderItemStatus(), 
-				new UpdateVendorBillItemsStatus(), new ViewAllCustomerSpecialOrdersByType()};
+				new UpdateVendorBillItemsStatus(), new ViewAllSpecialOrdersByType()};
 	
 
 		
