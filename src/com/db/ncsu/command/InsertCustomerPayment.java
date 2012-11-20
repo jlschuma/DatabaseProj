204a632e6c3aa9f2691767d1bdf71d4216dc91ba
Eric Lumpkin <eblumpki@ncsu.edu>
2012-11-19 21:49:17 -0500

    Added InsertCustomerPayment.java

---
+++ src/com/db/ncsu/command/InsertCustomerPayment.java
@@ -0,0 +1,33 @@
+package com.db.ncsu.command;
+
+import com.db.database.DatabaseManager;
+
+public class InsertCustomerPayment extends Command {
+
+	@Override
+	public CommandArgument[] getArguments() {
+		CommandArgument args[] = new CommandArgument[4];
+		args[0] = new CommandArgument("customerBillCycleID","Int","Customer Billing Cycle ID",true);		
+		args[1] = new CommandArgument("staffID","Int","Staff ID",true);
+		args[2] = new CommandArgument("Today","Date","Today",false);
+		args[3] = new CommandArgument("paymentInformation","String","Payment Information",true);
+		return args;
+	}
+
+	@Override
+	public void run(CommandArgument[] args) {
+		String sql = "INSERT INTO CustomerPayment(customerBillCycleID, staffID, paidDate, paymentInformation, confirmationCode) VALUES(?, ?, ?, ?, customer_confirmation_seq.nextval)";
+		DatabaseManager.runPreparedStatement(sql,args,false);
+		
+	}
+
+	@Override
+	public String getCommandName() {
+		return "Insert a Customer Payment";
+	}
+	
+	
+	
+
+	
+}
