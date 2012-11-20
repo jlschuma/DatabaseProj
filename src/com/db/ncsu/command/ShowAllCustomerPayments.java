564036c3a57b0eb87d79f9462fce642f0fcaa704
Eric Lumpkin <eblumpki@ncsu.edu>
2012-11-19 21:56:42 -0500

    Added ShowAllCustomerPayments.java

---
+++ src/com/db/ncsu/command/ShowAllCustomerPayments.java
@@ -0,0 +1,30 @@
+package com.db.ncsu.command;
+
+import com.db.database.DatabaseManager;
+
+public class ShowAllCustomerPayments extends Command {
+	public CommandArgument[] getArguments() {
+		CommandArgument args[] = new CommandArgument[1];
+		args[0] = new CommandArgument("customerID","Int","Customer ID",false);
+
+		return args;
+	}
+
+	@Override
+	public void run(CommandArgument[] args) {
+		//SELECT storeID, sum(price * quantity) AS totalBill
+		//FROM CustomerBillingCycle cb, CustomerBill b, CustomerBillItems bi
+		//WHERE b.id = bi.customerBillID AND cb.id = b.id AND cb.customerID = 1 AND dateTime > Ô01-JAN-03Õ AND dateTime < Ô01-JAN-04Õ AND status = ÔbilledÕ
+		//GROUP BY storeID
+		String sql = "SELECT customerBillCycleID, staffID, paidDate, paymentInformation, confirmationCode " +
+				"FROM CustomerPayment, CustomerBillingCycle" +
+				"WHERE customerID = ?";
+		DatabaseManager.runPreparedStatement(sql,args,true);
+		
+	}
+
+	@Override
+	public String getCommandName() {
+		return "Show all customer payments for a given customer.";
+	}
+}
