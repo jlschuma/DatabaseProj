0ce68ca95f049baab2483c8655ec22482c4eed8b
jlschuma <J_Saltz@hotmail.com>
2012-11-19 22:48:24 -0500

    Fixes

---
+++ src/com/db/ncsu/command/ViewACustomerSpecialOrders.java
@@ -0,0 +1,28 @@
+package com.db.ncsu.command;
+
+import com.db.database.DatabaseManager;
+
+public class ViewACustomerSpecialOrders extends Command {
+	public CommandArgument[] getArguments() {
+		CommandArgument args[] = new CommandArgument[1];
+		args[0] = new CommandArgument("Customer ID","Int","Customer ID",true);
+		return args;
+	}
+
+	@Override
+	public void run(CommandArgument[] args) {
+		//SELECT storeID, status, specialOrderID, merchandiseID, quantity, price
+		//FROM SpecialOrder o, SpecialOrderItems oi
+		//WHERE o.id = oi.specialOrderID AND customerID = 3
+		String sql = "select id,datetime,storeid,staffid,merchandiseid,quantity,price,status from SpecialOrder so, SpecialOrderItems soi "+
+		"where so.id = soi.specialorderid "+
+		"and customerId=? ";
+		DatabaseManager.runPreparedStatement(sql,args,true);
+		
+	}
+
+	@Override
+	public String getCommandName() {
+		return "View A Customers Special Orders";
+	}
+}
