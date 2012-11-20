8bcb65e091660f73ee3898802de1f9f4eb2e8347
brittjay0104 <bjohnson2011@gmail.com>
2012-11-19 22:18:23 -0500

    Update src/com/db/ncsu/command/ViewAllCustomerSpecialOrdersByType.java
    removed customerID (want to return ALL open orders for all customers)

--- src/com/db/ncsu/command/ViewAllCustomerSpecialOrdersByType.java
+++ src/com/db/ncsu/command/ViewAllCustomerSpecialOrdersByType.java
@@ -5,9 +5,8 @@ import com.db.database.DatabaseManager;
 public class ViewAllCustomerSpecialOrdersByType extends Command {
 	
 	public CommandArgument[] getArguments() {
-		CommandArgument args[] = new CommandArgument[2];
-		args[0] = new CommandArgument("Customer ID","Int","Customer ID",true);
-		args[1] = new CommandArgument("Status","String","Status",true);
+		CommandArgument args[] = new CommandArgument[1];
+		args[0] = new CommandArgument("Status","String","Status",true);
 		return args;
 	}
 
@@ -18,7 +17,7 @@ public class ViewAllCustomerSpecialOrdersByType extends Command {
 		//WHERE o.id = oi.specialOrderID AND customerID = 3
 		String sql = "SELECT storeID, specialOrderID, SUM(quantity * price) AS Balance " +
 				"FROM SpecialOrder o, SpecialOrderItems oi " +
-				"WHERE o.id = oi.specialOrderID AND customerID = ? AND status = ?" +
+				"WHERE o.id = oi.specialOrderID AND status = ?" +
 				"GROUP BY storeID, specialOrderID";
 		DatabaseManager.runPreparedStatement(sql,args,true);
 		
