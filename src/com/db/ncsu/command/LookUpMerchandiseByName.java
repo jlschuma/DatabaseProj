8209ea01e4036d3d1f90625f02bc1287f0d1d74b
jlschuma <J_Saltz@hotmail.com>
2012-11-19 20:20:30 -0500

    Changed Stocking Workflow

---
+++ src/com/db/ncsu/command/LookUpMerchandiseByName.java
@@ -0,0 +1,27 @@
+package com.db.ncsu.command;
+
+import com.db.database.DatabaseManager;
+
+public class LookUpMerchandiseByName extends Command {
+
+	@Override
+	public CommandArgument[] getArguments() {
+		CommandArgument args[] = new CommandArgument[1];
+		args[0] = new CommandArgument("Name","String","Name",false);
+		return args;
+	}
+
+	@Override
+	public void run(CommandArgument[] args) {
+		//Select * from Store
+		String sql = "Select * from Merchandise m, Vendor v where m.vendorId = v.id and m.name like ?";
+		DatabaseManager.runPreparedStatement(sql,args,true);
+			
+	}
+
+	@Override
+	public String getCommandName() {
+		return "Show all Stores";
+	}
+	
+}
