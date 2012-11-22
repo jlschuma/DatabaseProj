77f6d18ae33b10633c8487c70d19e28f29f3712d
Eric Lumpkin <eblumpki@ncsu.edu>
2012-11-22 16:28:51 -0500

    Added date ranges.

--- src/com/db/ncsu/command/ShowAllCustomerBillsByStore.java
+++ src/com/db/ncsu/command/ShowAllCustomerBillsByStore.java
@@ -4,7 +4,9 @@ import com.db.database.DatabaseManager;
 
 public class ShowAllCustomerBillsByStore extends Command {
 	public CommandArgument[] getArguments() {
-		CommandArgument args[] = new CommandArgument[0];
+		CommandArgument args[] = new CommandArgument[2];
+		args[0] = new CommandArgument("Start Date", "Date", "Start Date (MM/DD/YYYY)", true);
+		args[1] = new CommandArgument("End Date", "Date", "End Date (MM/DD/YYYY)", true);
 		return args;
 	}
 
@@ -18,7 +20,7 @@ public class ShowAllCustomerBillsByStore extends Command {
 		
 		String sql = "SELECT storeId, status, sum(price * quantity) AS TotalBalance " +
 				"FROM CustomerBillingCycle cb, CustomerBill b, CustomerBillItems bi "+
-				"WHERE cb.id = b.customerBillcycleid  AND b.id = bi.customerbillid " +
+				"WHERE cb.id = b.customerBillcycleid  AND b.id = bi.customerbillid AND DateTime > ? AND DateTime < ? " +
 				"GROUP BY storeId,Status ";
 
 		DatabaseManager.runPreparedStatement(sql,args,true);
--- src/com/db/ncsu/command/ShowAllVendorBills.java
+++ src/com/db/ncsu/command/ShowAllVendorBills.java
@@ -4,9 +4,10 @@ import com.db.database.DatabaseManager;
 
 public class ShowAllVendorBills extends Command {
 	public CommandArgument[] getArguments() {
-		CommandArgument args[] = new CommandArgument[1];
+		CommandArgument args[] = new CommandArgument[3];
 		args[0] = new CommandArgument("StoreID","Int","StoreID",false);
-
+		args[1] = new CommandArgument("Start Date", "Date", "Start Date (MM/DD/YYYY)", true);
+		args[2] = new CommandArgument("End Date", "Date", "End Date (MM/DD/YYYY)", true);
 		return args;
 	}
 
@@ -18,7 +19,7 @@ public class ShowAllVendorBills extends Command {
 		//GROUP BY vendorID,storeID
 		String sql = "SELECT vendorID, v.name, sum(price * quantity) AS TotalBalance " +
 				"FROM VendorBill vb, VendorBillItems vi, Vendor v " +
-				"WHERE vi.vendorBillID = vb.id AND vb.vendorID = v.id and StoreId = ?" +
+				"WHERE vi.vendorBillID = vb.id AND vb.vendorID = v.id and StoreId = ? AND DateTime > ? AND DateTime < ? " +
 				"GROUP BY vendorID, v.name";
 		DatabaseManager.runPreparedStatement(sql,args,true);
 		
